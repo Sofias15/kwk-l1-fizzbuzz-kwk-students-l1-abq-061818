20bc532521708084c883a64343de35da04b1a6a5
Sofias15 <anasofiasanch@gmail.com>
2018-06-19 20:30:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,15 +3,15 @@ def fizzbuzz(number)
 if number%15==0 
   puts "fizz"
  
- 
-elsif number%5==0
+ elsif number%5==0
 puts "buzz"
 
 elsif number%3==0
 puts "fizzbuzz"
+
 end 
 end 
 
-fizzbuzz(65)
+fizzbuzz(646462749483)
 
 
